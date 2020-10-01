727041a6f65ef2407c9791ffb1313fbf40068283
micolka <mic_la@tut.by>
2020-10-01 23:52:27 +0300

    feat: resolve morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+	let subArr = [];
+
+    for(let k = 0; k < expr.length / 10; k++) {
+    	arr.push(expr.substr(k * 10, 10));
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+    	subArr = [];
+    	for(let j = 0; j < arr[i].length; j += 2) {
+    		if (arr[i][j] === '1' && arr[i][j + 1] === '0') subArr.push('.');
+    		if (arr[i][j] === '1' && arr[i][j + 1] === '1') subArr.push('-');
+    	}    	
+    	arr[i] = MORSE_TABLE[subArr.join('')];
+    	if (!arr[i]) arr[i] = ' ';
+    }
+   return arr.join('');
 }
 
 module.exports = {
